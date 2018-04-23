8adc28f4f0e00ce81d991e32cf4a718b1822d43b
moon1638 <oohtet96@gmail.com>
2018-04-23 14:56:09 +0630

    exercise

---
+++ hello.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app=Flask(__name__)
+@app.route('/')
+def default():
+	return("Hello")
+	pass
+@app.route('/hello/<name>')
+def hello(name=None):
+	user={'name':name}
+	return render_template('hello.html',user=user)
+
+if __name__== "__main__":
+	app.run(host='0.0.0.0', debug=True)
