57ec780820bc8bff2bf23a3a0c33b347e9a0225c
moon1638 <oohtet96@gmail.com>
2018-04-25 10:36:22 +0630

    exercise

--- base.py
+++ base.py
@@ -1,15 +1,9 @@
 from flask import Flask,render_template
 app=Flask(__name__)
 
-@app.route('/inherits/')
-def inherits():
-	return render_template('base.html')
-@app.route('/inherits/one/')
-def inherits_one():
-	return render_template('in1.html')
-@app.route('/inherits/two/')
-def inherits_two():
-	return render_template('in2.html')
+@app.route('/')
+def root():
+	return render_template('b.html'),200
 
 if __name__=="__main__":
 	app.run(host='0.0.0.0',debug=True)
